99dd32a40b71d68e1d01bda9870b8a4407d04117
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2024-05-12 18:27:49 +0530

    fixing the error

--- src/Components/Top_Headlines/Headlines.jsx
+++ src/Components/Top_Headlines/Headlines.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import styles from "./Headlines.module.css"
+import React, { useEffect, useState } from 'react';
+import styles from "./Headlines.module.css";
 
 const Headlines = () => {
-  const [article, setArticle] = useState([]);
+  const [article, setArticle] = useState(null); // Initialize as null
+
   useEffect(() => {
     const apiKey = '9182c935c52e41e6be544709356d5da2';
     const URL = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`;
@@ -11,20 +12,19 @@ const Headlines = () => {
       .then(response => response.json())
       .then(data => {
         setArticle(data.articles);
-
       })
       .catch(error => {
         console.log(error);
       });
   }, []);
+
   return (
     <>
       <div className={styles.main}>
-        {article.length > 0 ? (
+        {article !== null && article !== undefined ? ( // Check if article is not null or undefined
           article.map((contexts, index) => (
             <div className='individuals' key={index}>
               <img src={contexts.urlToImage || "def.jpg"} onError={(e) => { e.target.src = 'def.jpg' }} />
-
               <div className='content-area'>
                 <div className='content-heading '>
                   <b>Heading:</b>
