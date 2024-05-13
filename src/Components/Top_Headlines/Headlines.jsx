9cd6dd6cb7bd96380b45121ff7c6ed5954b4b870
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2024-05-13 14:03:32 +0530

    error handling 03

--- src/Components/Top_Headlines/Headlines.jsx
+++ src/Components/Top_Headlines/Headlines.jsx
@@ -2,36 +2,34 @@ import React, { useEffect, useState } from 'react';
 import styles from "./Headlines.module.css";
 
 const Headlines = () => {
-  const [article, setArticle] = useState(['']); // Initialize as null
+  const [article, setArticle] = useState(null); // Initialize as null
+  const [politics, setPolitics] = useState(null); // Initialize as null
 
   useEffect(() => {
-    const apiKey = '9182c935c52e41e6be544709356d5da2';
-    const URL = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`;
 
-    fetch(URL)
-      .then(response => response.json())
+    const url = 'https://newsdata.io/api/1/news?apikey=pub_440474795d31aa774416df4707500b3b1633f&country=in&language=en&category=business';
+    fetch(url)
+      .then(fetchData => fetchData.json())
       .then(data => {
-        setArticle(data.articles);
+        setArticle(data.results);
       })
-      .catch(error => {
-        console.log(error);
-      });
   }, []);
 
   return (
     <>
+      {/* Business News */}
       <div className={styles.main}>
-        {article.length > 0 ? ( // Check if article is not null or undefined
+        {article && article.length > 1 ? ( // Check if article is not null or undefined
           article.map((contexts, index) => (
             <div className='individuals' key={index}>
-              <img src={contexts.urlToImage || "def.jpg"} onError={(e) => { e.target.src = 'def.jpg' }} />
+              <img src={contexts.image_url || "def.jpg"} onError={(e) => { e.target.src = 'def.jpg' }} />
               <div className='content-area'>
                 <div className='content-heading '>
                   <b>Heading:</b>
                   <p>{contexts.title}</p>
                 </div>
                 <div className='content-heading '>
-                  <b>Author: </b><p>{contexts.author || 'Unknown'}</p>
+                  <b>Author: </b><p>{contexts.source_id || 'Unknown'}</p>
                 </div>
               </div>
             </div>
